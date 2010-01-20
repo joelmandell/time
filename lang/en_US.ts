3fae039aee6485f2568d0fb17d5a74a63090c78d
Joel Mandell <joelmandell@gmail.com>
2010-01-20 16:12:33 +0100

    Internationalization in the workings....

---
+++ lang/en_US.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>TTimer</name>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="23"/>
+        <source>Tidtagningsverktyg för tal och framställningar.</source>
+        <translation>Timetracking for speeches</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="24"/>
+        <source>Välkommen till tidtagnings programmet! Här visas status-meddelanden.</source>
+        <translation>Welcome to the timetracking software. Here you can see the status.</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="37"/>
+        <source>Välj schema:</source>
+        <translation>Select schedule.</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="68"/>
+        <source>Starta</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="69"/>
+        <source>Stoppa</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="120"/>
+        <source>Konfiguering</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="124"/>
+        <source>Standard schema:</source>
+        <translation>Standard schedule:</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="126"/>
+        <source>Spara</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="181"/>
+        <source>&amp;Redigera</source>
+        <translation>Edit</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="188"/>
+        <source>&amp;Inställningar</source>
+        <translation>Properties</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="189"/>
+        <source>Ctrl+P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="190"/>
+        <source>Ändra inställningar för programmet.</source>
+        <translation>Change settings for the application</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="192"/>
+        <source>&amp;Nytt schema</source>
+        <translation>New schedule</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="279"/>
+        <source>min.</source>
+        <translation>min.</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="345"/>
+        <location filename="ttimer.cpp" line="375"/>
+        <source>Du kan bara flytta fram denna tid, eftersom ovanstående tid räknas.</source>
+        <translation>You can only move this item below, cause the above is counting.</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="347"/>
+        <location filename="ttimer.cpp" line="377"/>
+        <source>Du kan bara flytta fram denna tid, eftersom ovanstående tid har redan slutförts.</source>
+        <translation>You can only move this item below, case the above is already counted.</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="360"/>
+        <source>Senarelade tidtagning: </source>
+        <translation>Postponed item:</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="386"/>
+        <source>Tidigarelade tidtagning: </source>
+        <translation>Preponed item:</translation>
+    </message>
+    <message utf8="true">
+        <location filename="ttimer.cpp" line="417"/>
+        <source>Räknar!</source>
+        <translation>Counting!</translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="543"/>
+        <location filename="ttimer.cpp" line="544"/>
+        <source>%3 - %1:0%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ttimer.cpp" line="545"/>
+        <source>%3 - %1:%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
